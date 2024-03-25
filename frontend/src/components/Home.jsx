c483f8c6a44e85309f6459dc9bb69d8b1641d5a6
herminiocastro <hxssport@gmail.com>
2024-03-25 17:02:12 +0100

    inicialized Home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,18 +1,31 @@
-import "./Home.css" 
+import { useState, useEffect } from 'react';
+import './Home.css';
 
-<!DOCTYPE html>
+const Home = () => {
+  const [seconds, setSeconds] = useState(5);
 
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Stranger Pops World</title>
-  <link rel="stylesheet" href="styles/App.css">
-</head>
-<body>
-  <div id="root"></div>
-  <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
-  <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
-  <script src="app/index.js"></script>
-</body>
-</html>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds - 1);
+      if (seconds === 0) {
+        clearInterval(interval);
+        // Redirigir a la siguiente página
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="Home">
+      <div className="fondo-animado"></div>
+      <div className="logo">
+        <img src="./assets/logo.png" alt="Logo de la aplicación" />
+      </div>
+      <div className="temporizador">
+        {seconds}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "../components/Home.jsx";
+import { Principal } from "../components/Principal.jsx";
+import { Secundary } from "../components/Secundary.jsx";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/principal" element={<Principal />} />
+        <Route path="/secundary" element={<Secundary />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
