42d535425a7a11ea07f131ebb7c1084303ee8b75
herminiocastro <hxssport@gmail.com>
2024-04-07 21:29:09 +0200

    secundary init

--- frontend/src/components/Secundary.jsx
+++ frontend/src/components/Secundary.jsx
@@ -1,73 +1,59 @@
-import { useState, useEffect } from "react";
+
+import { useParams } from "react-router-dom";
 import "./Secundary.css";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
 import Logo from "../assets/STRANGER-POPS-WORLD-23-3-2024.png";
-import Boton1 from "../assets/boton1.svg";
-import Boton2 from "../assets/boton2.svg";
-import HomeImg from "../assets/HomeImg.png";
+import BotonVolver from "../assets/HomeImg.png";
+import axios from "axios";
+import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
-import datos from "../data/articles.json";
-
-export const Secundary = ({ index }) => {
-  console.log("El indice es: " + index);
-  const [tituloProducto, setTituloProducto] = useState(0);
-  const [descripcionProducto, setDescripcionProducto] = useState(0);
-  const [imagenesProducto, setImagenesProducto] = useState([]);
+export const Secundary = () => {
+  const [articulo, setArticulo] = useState([]);
+  const { id } = useParams();
 
   useEffect(() => {
-    const articulos = datos;
-    setTituloProducto(articulos[0].titulo);
-    setDescripcionProducto(articulos[0].descripcion);
-    setImagenesProducto(articulos[0].imagenes);
-  }, [0]);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/articulos/${id}`
+        );
+        setArticulo(response.data);
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+      }
+    };
 
-  if (imagenesProducto != undefined && !imagenesProducto.length) {
-    return <p>No hay datos disponibles para este producto.</p>;
-  }
+    fetchData();
+  }, [id]);
 
   return (
-    <div className="secundario">
+    <div className="Secundary">
       <header>
-        <img src={Logo} alt="Logo" className="logo" />
+        <img src={Logo} alt="Logo" className="Logo" />
       </header>
-      <section className="detalles-producto">
-        <div className="contenedor-imagenes">
-          {imagenesProducto.map((imagen, indice) => (
-            <img
-              key={indice}
-              src={imagen}
-              alt="Producto"
-              className="imagen-producto"
-            />
-          ))}
+
+      <div className="articulo">
+        {articulo.imagen && (
+          <img src={articulo.imagen} className="articuloImagen" />
+        )}
+        <div className="categoria">
+          <p>{articulo.categoria}</p>
         </div>
-        <div className="botones">
-          <Link to="/editar" className="boton-editar">
-            Editar
-          </Link>
-          <Link to="/" className="boton-eliminar">
-            Eliminar
-          </Link>
+
+        <div className="titulo">
+          <h2>{articulo.titulo}</h2>
         </div>
-        <h1>{tituloProducto}</h1>
-        <p>{descripcionProducto}</p>
-      </section>
-      <footer>
-        <Link to="/">
-          <img src={Boton1} alt="Buscar" />
+
+        <div className="descripcion">
+          <p>{articulo.descripcion}</p>
+        </div>
+      </div>
+
+      <div className="botonVolver">
+        <Link to="/Principal/*">
+          <img src={BotonVolver} alt="Botón Volver" />
         </Link>
-        <Link to="/">
-          <img src={HomeImg} alt="Inicio" />
-        </Link>
-        <Link to="/">
-          <img src={Boton2} alt="Perfil" />
-        </Link>
-      </footer>
+      </div>
     </div>
   );
-};
-
-Secundary.propTypes = {
-  index: PropTypes.number.isRequired,
 };
