794f1d60ccc94199ff74021427d0c70e9b44fea7
Javi <javi@jsrois.net>
2024-04-11 16:12:05 +0200

    Revert "initiated test"
    This reverts commit 69b8cd054b059fed29b060bc03c32621ed47cd1f.

--- frontend/src/__test__/Home.test.jsx
+++ frontend/src/__test__/Home.test.jsx
@@ -1,32 +1,14 @@
 import { describe,it, expect } from 'jest'; 
-import { render, screen, fireEvent } from '@testing-library/react';
-import  Home  from './Home';
+import { render, screen } from '@testing-library/react';
+import { Home } from './Home';
 
-describe('<Home />', () => {
-  test('Renderiza correctamente el componente', () => {
-    render( Home );
+describe('Home Component', () => {
+  it('Renderiza correctamente el componente', () => {
+    render(<Home />);
     const enterButton = screen.getByText('ENTER');
     expect(enterButton).toBeInTheDocument();
   });
-  });
+});
 
-describe("<Counter />", () => {
-  test("renders the counter component", () => {
-    render( Counter );
-    const countElement = screen.getByText(/Count:/i);
-    const buttonElement = screen.getByText(/Increment:/i);
 
-    expect(countElement).toBeInTheDocument();
-    expect(buttonElement).toBeInTheDocument();
-  });
-
-  test("increments the count when the button is clicked", () => {
-    render( Counter );
-    const buttonElement = screen.getByText(/Increment/i);
-
-    fireEvent.click(buttonElement);
-    const countElement = screen.getByText(/Count: 1/i);
-
-    expect(countElement).toBeInTheDocument();
-  });
 
