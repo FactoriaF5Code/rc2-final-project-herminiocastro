6cc02b9679f07db329ec752036bd4fbc7bb96f4e
herminiocastro <hxssport@gmail.com>
2024-04-15 21:23:27 +0200

    formulario mas cambios

--- frontend/src/components/CreationForm.jsx
+++ frontend/src/components/CreationForm.jsx
@@ -2,25 +2,47 @@ import { useState } from 'react';
 
  export const CreationForm = () => {
   const [image, setImage] = useState(null);
-  const [category, setCategory] = useState('');
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const options = [ { value: 'POPS', label: 'POPS' }, { value: 'VARIOS', label: 'VARIOS' } ];
+  const [categoryValue , setCategoryValue] = useState ('POPS')
+
+  
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    console.log("Image:", image);
+    console.log("Title:", title);
+    console.log("Description:", description);
+    const formData = {
+      image,
+      categoryValue,
+      title,
+      description
+    };
+    console.log("Form Data:", formData);
   };
+
+  const handleSelectedChange = async (event) => {
+    event.preventDefault();
+    setCategoryValue(event.target.value);
+  };
+  
+  
 
   return (
     <form onSubmit={handleSubmit}>
       <input 
-        type="file" 
-        accept="image/*" 
+        type="file"
         onChange={(e) => setImage(e.target.files[0])} 
       />
-      <select value={category} onChange={(e) => setCategory(e.target.value)}>
-        <option value="">Selecciona una categoría</option>
-        {}
+      <select id="select" value={categoryValue} onChange={handleSelectedChange}>
+        {options.map(option => (
+            <option key={option.value} value={option.value}>
+            {option.label}
+            </option>
+        ))}
       </select>
       <input 
         type="text" 
