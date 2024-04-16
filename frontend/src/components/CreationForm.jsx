057a54ba8c5358d5c98d97a011b76d05ffc8c887
herminiocastro <hxssport@gmail.com>
2024-04-16 16:23:34 +0200

    formulario envia datos pero llegan como null

--- frontend/src/components/CreationForm.jsx
+++ frontend/src/components/CreationForm.jsx
@@ -1,64 +1,74 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../components/CreationForm.css";
 
- export const CreationForm = () => {
+export const CreationForm = () => {
   const [image, setImage] = useState(null);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const options = [ { value: 'POPS', label: 'POPS' }, { value: 'VARIOS', label: 'VARIOS' } ];
-  const [categoryValue , setCategoryValue] = useState ('POPS')
-
-  
-
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const navigate = useNavigate();
+  const options = [
+    { value: "POPS", label: "POPS" },
+    { value: "VARIOS", label: "VARIOS" },
+  ];
+  const [categoryValue, setCategoryValue] = useState("POPS");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("Image:", image);
-    console.log("Title:", title);
-    console.log("Description:", description);
+
     const formData = {
       image,
       categoryValue,
       title,
-      description
+      description,
     };
-    console.log("Form Data:", formData);
+
+    try {
+      const response = await axios.post("http://localhost:8080/api/articulos/", formData);
+      if (response.status === 200) {
+        alert("¡Publicación creada con éxito!");
+        navigate("/Principal/");
+      } else {
+        alert("Error al crear la publicación. Inténtalo de nuevo.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Error al crear la publicación. Comprueba la consola para más información.");
+    }
   };
 
   const handleSelectedChange = async (event) => {
     event.preventDefault();
     setCategoryValue(event.target.value);
   };
-  
-  
 
   return (
     <form onSubmit={handleSubmit}>
-      <input 
+      <input
+        className="file"
         type="file"
-        onChange={(e) => setImage(e.target.files[0])} 
+        onChange={(e) => setImage(e.target.files[0])}
       />
       <select id="select" value={categoryValue} onChange={handleSelectedChange}>
-        {options.map(option => (
-            <option key={option.value} value={option.value}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
             {option.label}
-            </option>
+          </option>
         ))}
       </select>
-      <input 
-        type="text" 
-        value={title} 
-        onChange={(e) => setTitle(e.target.value)} 
-        placeholder="Título" 
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Título"
       />
-      <textarea 
-        value={description} 
-        onChange={(e) => setDescription(e.target.value)} 
-        placeholder="Descripción" 
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Descripción"
       />
       <button type="submit">Guardar</button>
     </form>
   );
 };
- 
-
