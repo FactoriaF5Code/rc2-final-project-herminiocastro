e28248969fcb96132509b43eba3bf46f0ff51f39
herminiocastro <hxssport@gmail.com>
2024-04-16 16:44:34 +0200

    creados botones editar y eliminar en secundary

--- frontend/src/components/Secundary.jsx
+++ frontend/src/components/Secundary.jsx
@@ -4,11 +4,12 @@ import Logo from "../assets/STRANGER-POPS-WORLD-23-3-2024.png";
 import BotonVolver from "../assets/HomeImg.png";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link ,useNavigate } from "react-router-dom";
 
 export const Secundary = () => {
   const [elemento, setelemento] = useState([]);
   const { index } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,6 +40,10 @@ export const Secundary = () => {
       <div className="categoriaSecond">
         <p>{elemento.categoria}</p>
       </div>
+      <div className="botonesEdicion">
+          <button onClick={() => navigate(`/Editar/${index}`)}>EDITAR</button>
+          <button onClick={() => handleEliminar()}>ELIMINAR</button>
+        </div>
 
       <div className="tituloSecond">
         <h2>{elemento.titulo}</h2>
@@ -58,4 +63,17 @@ export const Secundary = () => {
       </div>
     </div>
   );
+};
+
+const handleEliminar = async () => {
+  const { index } = useParams();
+
+  try {
+    await axios.delete(`http://localhost:8080/api/articulos/${index}`);
+    alert("Artículo eliminado con éxito!");
+    navigate("/Principal/"); // Redirect to the main page
+  } catch (error) {
+    console.error("Error al eliminar el artículo:", error);
+    alert("Error al eliminar el artículo. Inténtalo de nuevo.");
+  }
 };
